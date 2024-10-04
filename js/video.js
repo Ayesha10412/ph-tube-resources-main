bc4589fa2e0eedb55ecd770e178f8f354b544715
Your Name <k.bristi057@gmail.com>
2024-10-04 22:21:53 +0600

    added some new files

---
+++ js/video.js
@@ -0,0 +1,93 @@
+// fetch, load and show categories on html
+
+const loadCategories = () =>{
+
+fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
+.then(response => response.json())
+.then(data => displayCategories(data.categories))
+.catch(error => console.log(error))
+
+}
+
+
+// display categories
+
+const displayCategories = (categories) =>{
+
+const categoryContainer = document.getElementById("categories")
+
+categories.forEach(item => {
+    console.log(item)
+
+    // create a btn
+const button = document.createElement('button')
+button.classList ="btn"
+button.innerText= item.category
+ categoryContainer.append(button)
+
+});
+
+}
+
+
+
+
+
+// create videos
+
+const loadVideos = () =>{
+
+    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+    .then(response => response.json())
+    .then(data => displayCategoriesVideos(data.videos))
+    .catch(error => console.log(error))
+    
+    }
+    
+
+    const displayCategoriesVideos = (videos) =>{
+
+        const categoryContainerVideos = document.getElementById("videos")
+        
+        videos.forEach(video => {
+            console.log(video)
+        
+            const card = document.createElement("div")
+            card.classList= "card card-compact"
+            card.innerHTML = `
+            
+            <figure class=" ">
+    <img class="w-full h-[200px] object-cover"
+      src="${video.thumbnail}"
+      alt="" />
+  </figure >
+  <div class="px-0 py-2 flex gap-2">
+    <div> 
+<img class="w-10 h-10 rounded-xl object-cover" src="${video.authors[0].profile_picture
+}" alt="">
+    </div>
+ <div> 
+ <h2 class="font-bold text-text text-xl">${video.title}</h2>
+<div class="flex gap-3"> <p class="text-gray text-sm">${video.authors[0].profile_name}</p>
+<img class="w-5 h-5 " src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png">
+</div>
+
+ <p class="text-gray text-xs">${video.others.views}      views</p>
+
+    </div>
+
+  </div>
+                 
+            `
+
+            categoryContainerVideos.append(card)
+
+
+        });
+        
+        }
+
+
+loadCategories()
+
+loadVideos()
